bb6b9cbadaa9467e0e5d6e94b2bc94dd7b6b0ba6
Joseba S <jjoseba@gmail.com>
2017-05-18 18:37:01 +0200

    Creada primera vista de bandas y modelos de evento y escenario

--- bands/models.py
+++ bands/models.py
@@ -8,8 +8,8 @@ class Band(models.Model):
     name = models.CharField(null=False, verbose_name='Nombre', max_length=240)
     tag = models.CharField(null=False, verbose_name='etiqueta', max_length=140)
     genre = models.CharField(null=False, verbose_name='etiqueta', max_length=240)
-    profile_image = models.ImageField(null=True)
-    band_image = models.ImageField(null=True)
+    profile_image = models.ImageField(null=True, blank=True)
+    band_image = models.ImageField(null=True, blank=True)
     city = models.CharField(null=False, verbose_name='Ciudad', max_length=140)
     num_members = models.IntegerField(default=1)
     description = models.TextField(null=True, blank=True)
@@ -23,3 +23,26 @@ class Band(models.Model):
 
     def __unicode__(self):
         return self.name
+
+
+class Venue(models.Model):
+    name = models.CharField(null=False, verbose_name='Nombre', max_length=240)
+    description = models.TextField(null=False, blank=True)
+    latitude = models.FloatField(null=False)
+    longitude = models.FloatField(null=False)
+    image = models.ImageField(null=True, blank=True)
+    address = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Escenario'
+        verbose_name_plural = 'Escenarios'
+        ordering = ['name']
+
+    def __unicode__(self):
+        return self.name
+
+class Event(models.Model):
+    band = models.ForeignKey(Band, related_name="band")
+    venue = models.ForeignKey(Venue, related_name="venue")
+    day = models.DateField(null=False)
+    time = models.TimeField(null=False)
--- bands/views.py
+++ bands/views.py
@@ -4,3 +4,10 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+from bands.models import Band
+
+
+def index(request):
+    bands = Band.objects.all()
+
+    return render(request, 'index.html', { 'bands': bands })
