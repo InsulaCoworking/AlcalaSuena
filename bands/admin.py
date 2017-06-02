5592199c665c58c3ca497925cce2e6e07ff74af1
Joseba S <jjoseba@gmail.com>
2017-06-02 15:14:33 +0200

    Añadidas noticias al panel de admin

--- bands/admin.py
+++ bands/admin.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 from bands.models import Band, Venue, Event, Tag, BandToken, Settings
+from bands.models.news import News
 
 admin.site.register(Band)
 admin.site.register(Venue)
@@ -10,3 +11,4 @@ admin.site.register(Event)
 admin.site.register(Tag)
 admin.site.register(BandToken)
 admin.site.register(Settings)
+admin.site.register(News)
