eed4312335520152de06dca008b6010f6109e21c
Joseba S <jjoseba@gmail.com>
2017-06-07 13:58:41 +0200

    Añadido placeholder en el campo de importe de las facturas

--- bands/forms/billing_info.py
+++ bands/forms/billing_info.py
@@ -8,3 +8,6 @@ class BillingForm(forms.ModelForm):
     class Meta:
         model = BillingInfo
         exclude=[]
+        widgets = {
+            'billing_total': forms.NumberInput(attrs={'placeholder': '0,00'}),
+        }
