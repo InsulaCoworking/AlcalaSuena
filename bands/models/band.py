d1df9ad45b1dc5938538eb5599e7a4c9c6499ecf
Joseba S <jjoseba@gmail.com>
2018-04-18 13:08:33 +0200

    Añadida accion de admin para añadir bandas al cartel definitivo

--- bands/models/band.py
+++ bands/models/band.py
@@ -2,7 +2,8 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from imagekit.models import ProcessedImageField, ImageSpecField
+from pilkit.processors import ResizeToFit, ResizeToFill
 from bands.helpers import RandomFileName
 from bands.models import Tag
 
@@ -11,18 +12,31 @@ class Band(models.Model):
     name = models.CharField(null=False, verbose_name='Nombre', max_length=240)
     tag = models.ForeignKey(Tag, related_name="band_tag")
     genre = models.CharField(null=True, blank=True, verbose_name='etiqueta', max_length=240)
-    profile_image = models.ImageField(null=True, blank=True, upload_to=RandomFileName('band/'), verbose_name='Imagen principal')
-    band_image = models.ImageField(null=True, blank=True, upload_to=RandomFileName('band/'), verbose_name='Imagen de cabecera')
+    profile_image = ProcessedImageField(null=True, blank=True, upload_to=RandomFileName('band/'),
+                                        processors=[ResizeToFit(512, 512, upscale=False)], format='JPEG',
+                                        verbose_name='Imagen principal')
+    band_image = ProcessedImageField(null=True, blank=True, upload_to=RandomFileName('band/'),
+                                     processors=[ResizeToFit(1200, 600, upscale=False)], format='JPEG',
+                                     verbose_name='Imagen de cabecera')
+    profile_thumbnail = ImageSpecField(source='profile_image',
+                                       processors=[ResizeToFill(150, 150, upscale=False)],
+                                       format='JPEG',
+                                       options={'quality': 70})
     city = models.CharField(null=True, blank=True, verbose_name='Ciudad', max_length=140)
     num_members = models.IntegerField(null=True, blank=True, default=1)
     description = models.TextField(null=True, blank=True, verbose_name='Descripción')
-    embed_code = models.TextField(null=True, blank=True)
+    embed_code = models.TextField(null=True, blank=True,
+                                  verbose_name='Códido embed (enlace o iframe) para escucha (Bandcamp, Soundcloud, Spotify...)')
+    embed_media = models.TextField(null=True, blank=True,
+                                   verbose_name='Códido embed (enlace o iframe) de vídeo (Youtube, Vimeo...)')
 
     facebook_link = models.CharField(null=True, blank=True, verbose_name='Página de Facebook', max_length=250)
     youtube_link = models.CharField(null=True, blank=True, verbose_name='Canal de Youtube', max_length=250)
     twitter_link = models.CharField(null=True, blank=True, verbose_name='Perfil de Twitter', max_length=250)
     bandcamp_link = models.CharField(null=True, blank=True, verbose_name='Página de BandCamp', max_length=250)
     presskit_link = models.CharField(null=True, blank=True, verbose_name='Presskit', max_length=250)
+    webpage_link = models.CharField(null=True, blank=True, verbose_name='Página web', max_length=250)
+    spotify_link = models.CharField(null=True, blank=True, verbose_name='Perfil de Spotify', max_length=250)
 
     class Meta:
         verbose_name = 'Banda'
--- contest/admin.py
+++ contest/admin.py
@@ -1,8 +1,58 @@
+# coding=utf-8
 from django.contrib import admin
 
 # Register your models here.
+from bands.models import Band
 from contest.models import BandMember, ContestBand, ContestJuryVote
 
-admin.site.register(ContestBand)
+
+def make_winner(self, request, queryset):
+
+    bands_added = 0
+    for band in queryset:
+        band_exists = Band.objects.filter(name=band.name).count() > 0
+        if band_exists:
+            self.message_user(request, "'%s' ya se encontraba en el cartel definitivo." % band.name)
+        else:
+            new_band = Band.objects.create(
+                name=band.name,
+                city=band.city,
+                genre=band.genre,
+                tag=band.tag,
+                profile_image=band.profile_image,
+                band_image=band.band_image,
+                num_members=band.num_members,
+                description=band.description,
+                embed_code=band.embed_code,
+                embed_media=band.embed_media,
+                facebook_link=band.facebook_link,
+                youtube_link=band.youtube_link,
+                twitter_link=band.twitter_link,
+                bandcamp_link=band.bandcamp_link,
+                presskit_link=band.presskit_link,
+                webpage_link=band.webpage_link,
+                spotify_link=band.spotify_link,
+            )
+            new_band.save()
+            bands_added+=1
+
+    if bands_added > 0:
+        if bands_added == 1:
+            message_bit = "Se ha añadido una nueva banda al cartel definitivo."
+        else:
+            message_bit = "Se añadieron %s nuevas bandas al cartel definitivo." % bands_added
+
+        self.message_user(request, message_bit)
+
+make_winner.short_description = "Añadir banda(s) al cartel definitivo"
+
+
+class BandAdmin(admin.ModelAdmin):
+    list_display = ['name', 'genre', 'city', 'num_members', 'has_local_member']
+    ordering = ['name']
+    actions = [make_winner]
+
+
+admin.site.register(ContestBand, BandAdmin)
 admin.site.register(BandMember)
 admin.site.register(ContestJuryVote)
