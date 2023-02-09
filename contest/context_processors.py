487320aa34d8e052747c2c50bd121d6ba42a878e
Joseba S <jjoseba@gmail.com>
2023-02-09 13:46:56 +0100

    Modify view and settings for contest not opened yet

--- contest/context_processors.py
+++ contest/context_processors.py
@@ -5,6 +5,7 @@ def contest_status(request):
     return {
         'CONTEST_ACTIVE': settings.CONTEST_ACTIVE,
         'CONTEST_CLOSED': settings.CONTEST_CLOSED,
+        'CONTEST_OPENING': settings.CONTEST_OPENING,
         'APP_VISIBLE': settings.APP_VISIBLE,
         'ARCHIVE_YEARS': settings.ARCHIVE_YEARS,
         'SCHEDULE_ACTIVE':settings.SCHEDULE_ACTIVE,
--- contest/views.py
+++ contest/views.py
@@ -44,6 +44,9 @@ def signup(request):
     if settings.CONTEST_CLOSED:
         return render(request, 'contest/form_closed.html', {})
 
+    if settings.CONTEST_OPENING:
+        return render(request, 'contest/form_opening.html', {})
+
     members_factory = BandMemberForm.getMembersFormset()
 
     if request.method == "POST":
